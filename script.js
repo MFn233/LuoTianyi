f1e8dfbff74f4b7eb5640410018d3f7527e7166f
MFn233 <103323756+MFn233@users.noreply.github.com>
2022-08-22 17:39:37 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 function countdown() {
-    var now1 = new Date(); //获取服务器时间
-    var end = new Date((now.getFullYear() + 1) + "/7/12 20:00:00"); //结束时间
-    var now = now1.replace(/\-/g, "/"); //将“-”替换为“/“，以此修复safari显示问题
+    var system = {};
+    var p = navigator.platform;
+    system.win = p.indexOf("Win") == 0;
+    system.mac = p.indexOf("Mac") == 0;
+    system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
+
+    var now = new Date(); //获取服务器时间
+
+    if (system.mac) { //草你妈的safari和ie，你妈死了
+        //为了这俩浏览器花了我一下午时间+一上午时间写兼容性代码
+        var end = new Date((now.getFullYear() + 1) + "/7/12 20:00:00");
+        var now = now1.replace(/\-/g, "/"); //将“-”替换为“/“，以此修复safari显示问题
+        console.log("Is Mac OS");
+    } else if (system.win) {
+        var end = new Date((now.getFullYear() + 1) + "-7-12 20:00:00"); //结束时间
+        console.log("Is Windows");
+    } else if (system.xll) {
+        var end = new Date((now.getFullYear() + 1) + "-7-12 20:00:00"); //结束时间
+        console.log("Is Linux");
+    } else {
+        console.log("Mobile");
+        var a = navigator.userAgent;
+        var isAndroid = a.indexOf('Android') > -1 || a.indexOf('Adr') > -1; //android终端
+        var isiOS = !!a.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+        if (isAndroid) {
+            console.log("Android as Chrome");
+            var end = new Date((now.getFullYear() + 1) + "-7-12 20:00:00"); //结束时间
+        }
+        if (isiOS) {
+            console.log("Is iOS, Safari");
+            var end = new Date((now.getFullYear() + 1) + "/7/12 20:00:00");
+            var now = now1.replace(/\-/g, "/"); //将“-”替换为“/“，以此修复safari显示问题
+        }
+    }
 
     var m = Math.round((end - now) / 1000); //服务器时间减去结束时间
     var day = parseInt(m / 24 / 3600); //计算时间
